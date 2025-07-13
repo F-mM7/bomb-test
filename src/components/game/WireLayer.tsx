c3f8565ab5c906f8f40093d8b148a1ff9b622834
F-mM7 <fmolljazz@gmail.com>
2025-07-13 19:27:35 +0900

    feat: implement wire path generation and update WireLayer component for dynamic rendering

--- src/components/game/WireLayer.tsx
+++ src/components/game/WireLayer.tsx
@@ -3,7 +3,111 @@ import SvgWire from "../common/SvgWire";
 import { scaleSize, BASE_SIZES } from "../../utils/responsive";
 import { Z_INDEX } from "../../styles/constants/zIndex";
 
+// ワイヤー生成関数
+type WireSide = "top" | "left" | "right" | "bottom";
+
+interface WireConfig {
+  side: WireSide;
+  startPos: number; // 0-1の範囲で辺上の位置を指定
+  endPos: number; // 0-1の範囲で辺上の位置を指定
+  color: string;
+  detour?: number; // 迂回の深さ（省略時は自動計算）
+}
+
+const generateWirePath = (config: WireConfig): string => {
+  const { side, startPos, endPos, detour } = config;
+
+  // ビューボックス範囲 (920x1000)
+  const deviceBounds = {
+    left: 120,
+    right: 800,
+    top: 110,
+    bottom: 890,
+  };
+
+  const defaultDetour = 80;
+  const actualDetour = detour ?? defaultDetour;
+
+  let startX: number, startY: number, endX: number, endY: number;
+  let control1X: number,
+    control1Y: number,
+    control2X: number,
+    control2Y: number;
+
+  switch (side) {
+    case "top":
+      startX =
+        deviceBounds.left + (deviceBounds.right - deviceBounds.left) * startPos;
+      endX =
+        deviceBounds.left + (deviceBounds.right - deviceBounds.left) * endPos;
+      startY = endY = deviceBounds.top;
+      control1X = startX;
+      control1Y = deviceBounds.top - actualDetour;
+      control2X = endX;
+      control2Y = deviceBounds.top - actualDetour;
+      break;
+
+    case "bottom":
+      startX =
+        deviceBounds.left + (deviceBounds.right - deviceBounds.left) * startPos;
+      endX =
+        deviceBounds.left + (deviceBounds.right - deviceBounds.left) * endPos;
+      startY = endY = deviceBounds.bottom;
+      control1X = startX;
+      control1Y = deviceBounds.bottom + actualDetour;
+      control2X = endX;
+      control2Y = deviceBounds.bottom + actualDetour;
+      break;
+
+    case "left":
+      startY =
+        deviceBounds.top + (deviceBounds.bottom - deviceBounds.top) * startPos;
+      endY =
+        deviceBounds.top + (deviceBounds.bottom - deviceBounds.top) * endPos;
+      startX = endX = deviceBounds.left;
+      control1X = deviceBounds.left - actualDetour;
+      control1Y = startY;
+      control2X = deviceBounds.left - actualDetour;
+      control2Y = endY;
+      break;
+
+    case "right":
+      startY =
+        deviceBounds.top + (deviceBounds.bottom - deviceBounds.top) * startPos;
+      endY =
+        deviceBounds.top + (deviceBounds.bottom - deviceBounds.top) * endPos;
+      startX = endX = deviceBounds.right;
+      control1X = deviceBounds.right + actualDetour;
+      control1Y = startY;
+      control2X = deviceBounds.right + actualDetour;
+      control2Y = endY;
+      break;
+  }
+
+  return `M ${startX} ${startY} C ${control1X} ${control1Y} ${control2X} ${control2Y} ${endX} ${endY}`;
+};
+
 const WireLayer: React.FC = () => {
+  // ワイヤー設定配列
+  const wireConfigs: WireConfig[] = [
+    { side: "top", startPos: 0.1, endPos: 0.6, color: "#0ff", detour: 50 },
+    { side: "top", startPos: 0.7, endPos: 0.95, color: "#f0f", detour: 125 },
+    { side: "left", startPos: 0.15, endPos: 0.35, color: "#ff0", detour: 120 },
+    { side: "left", startPos: 0.25, endPos: 0.65, color: "#fff", detour: 80 },
+    { side: "left", startPos: 0.55, endPos: 0.85, color: "#8f8", detour: 110 },
+    { side: "right", startPos: 0.2, endPos: 0.45, color: "#f88", detour: 60 },
+    { side: "right", startPos: 0.5, endPos: 0.55, color: "#80f", detour: 130 },
+    { side: "right", startPos: 0.65, endPos: 0.9, color: "#8ff", detour: 110 },
+    { side: "bottom", startPos: 0.25, endPos: 0.75, color: "#aaf", detour: 65 },
+    {
+      side: "bottom",
+      startPos: 0.125,
+      endPos: 0.375,
+      color: "#8fa",
+      detour: 50,
+    },
+  ];
+
   return (
     <svg
       viewBox="0 0 920 1000"
@@ -17,35 +121,13 @@ const WireLayer: React.FC = () => {
         zIndex: Z_INDEX.WIRE_LAYER,
       }}
     >
-      <SvgWire d="M 380 180 Q 380 60 460 60 Q 540 60 540 180" stroke="#d00" />
-
-      <SvgWire d="M 450 180 Q 450 40 530 40 Q 610 40 610 180" stroke="#00d" />
-
-      <SvgWire d="M 140 220 Q 20 220 20 260 Q 20 300 140 300" stroke="#dd0" />
-
-      <SvgWire d="M 140 360 Q 60 360 60 380 Q 60 400 140 400" stroke="#0a0" />
-
-      <SvgWire d="M 140 380 Q 40 380 40 400 Q 40 420 140 420" stroke="#a0a" />
-
-      <SvgWire
-        d="M 780 360 Q 880 360 880 380 Q 880 400 780 400"
-        stroke="#a0a"
-      />
-
-      <SvgWire
-        d="M 780 450 Q 900 450 900 490 Q 900 530 780 530"
-        stroke="#dd0"
-      />
-
-      <SvgWire
-        d="M 350 900 Q 350 950 430 950 Q 510 950 510 900"
-        stroke="#dd0"
-      />
-
-      <SvgWire
-        d="M 450 900 Q 450 970 530 970 Q 610 970 610 900"
-        stroke="#f80"
-      />
+      {wireConfigs.map((config, index) => (
+        <SvgWire
+          key={index}
+          d={generateWirePath(config)}
+          stroke={config.color}
+        />
+      ))}
     </svg>
   );
 };
