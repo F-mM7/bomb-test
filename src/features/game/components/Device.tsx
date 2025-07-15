9760fdf29677911ea5380ae388078c24d053a20a
F-mM7 <fmolljazz@gmail.com>
2025-07-15 23:25:50 +0900

    fix: adjust keyboard position calculation for improved layout

--- src/features/game/components/Device.tsx
+++ src/features/game/components/Device.tsx
@@ -121,8 +121,11 @@ const Device: React.FC<DeviceProps> = ({
           <div
             style={{
               position: "relative",
-              top: scaleSize(-BASE_SIZES.KEYBOARD_INNER_HEIGHT - 25),
-              left: scaleSize(7.4),
+              top: scaleSize(
+                -BASE_SIZES.KEYBOARD_INNER_HEIGHT -
+                  BASE_SIZES.CONTAINER_PADDING * 2
+              ),
+              left: "0",
               zIndex: Z_INDEX.KEYBOARD,
               transform:
                 animationPhase === "moving" ||
